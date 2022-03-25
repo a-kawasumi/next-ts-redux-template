07ab03e1fd736f7f370c4203a78145e9355c81a8
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-03-25 20:10:35 +0900

    fix: use get pokemon by name query

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { pokemonApi } from "~/services/sample";
 import counterReducer from "./counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
