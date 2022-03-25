a8b25b033e5083999bdc16494f00feafe3bec015
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-03-25 15:35:09 +0900

    chore: install stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,6 +5,17 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
+const buildStylelintCommand = (filenames) =>
+  `stylelint --fix ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+  "*.css": [buildStylelintCommand],
 };
